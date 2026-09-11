d50ad17a6742d42cc1b9e42a29b01cdaa33876af
jovemjededaia-cell <jovemjededaia@gmail.com>
2026-09-11 00:06:17 -0300

    feat: add game state

---
+++ core/GameState.js
@@ -0,0 +1,16 @@
+export class GameState {
+  constructor() { this.reset(); }
+
+  reset() {
+    this.running = false;
+    this.gameOver = false;
+    this.score = 0;
+    this.time = 0;
+    this.wave = 1;
+    this.spawnTimer = 0;
+    this.enemies = [];
+    this.projectiles = [];
+    this.effects = [];
+    this.player = null;
+  }
+}
