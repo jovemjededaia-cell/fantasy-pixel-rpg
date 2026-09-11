3f2bf255a11c8fab9de37979e859685d688408c3
jovemjededaia-cell <jovemjededaia@gmail.com>
2026-09-11 00:36:12 -0300

    feat: add combat configuration

---
+++ data/combat.js
@@ -0,0 +1,12 @@
+export const COMBAT_CONFIG = Object.freeze({
+  player: {
+    range: 420,
+    projectileSpeed: 560,
+    projectileRadius: 5,
+    baseCritChance: 0.08,
+    critMultiplier: 1.75,
+    knockback: 38,
+    invulnerabilityAfterHit: 0.35
+  },
+  hitstun: 0.08
+});
